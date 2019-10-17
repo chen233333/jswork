dda07490747c64ed43e136433be4e0a96d025b65
chen233333 <858788284@qq.com>
2019-10-17 11:37:05 +0800

    dsa

---
+++ work5/work5.js
@@ -0,0 +1,35 @@
+
+//
+str ='<table border="1">'
+for(let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table1').innerHTML = str
+       
+
+str ='<table border="1">'
+for(let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table2').innerHTML = str
+//倒九九
+str ='<table border="1">'
+for(let i=9;i>0;--i){
+    str +='<tr>'
+    for(var j=i;j>0;--j){
+        str += '<td>&nbsp;</td>'
+    }
+    str +='</tr>'
+}
+str +='</table>'
+document.getElementById('table3').innerHTML = str
