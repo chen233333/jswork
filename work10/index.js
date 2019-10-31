30fdb625a65e904a5459566f376670e9f2cd3615
chen233333 <858788284@qq.com>
2019-10-31 08:11:44 +0800

    10

--- work10/index.js
+++ work10/index.js
@@ -20,5 +20,28 @@ function monkey(){
          }
      }
      document.getElementById('monkeyking').innerText=monkey[0]
+}
+function stat(){
+    let str=document.getElementById('str').value
+   
+    // 1.用split()把字符串切分成数组
+    // 2.建议使用数组的reduce回调函数统计，也可自行使用数组统计。
+    // 使用JSON.stringfy()函数把对象转换成字符串显示到id为result的div中
 
-    }
+    
+    let obj = str.split('').sort().reduce(
+        (r, e) => (r[e]++ || (r[e] = 1), r), {}
+        )
+    console.log(obj)
+ 
+        // for (let i = 1;i < obj.length; i++){
+        //     for (let j =  i ; j > 0 ; j--){
+        //         if (obj[j-1]>obj[j]) {
+        //             [obj[j - 1],obj[j]] = [obj[j],obj[j-1]]
+        //         }
+        //     }
+        // }
+        // return obj
+    
+    document.getElementById('result').innerText=JSON.stringify(obj)
+}
