a42dc83e670eb505294e40925e7a75788896de13
chen233333 <858788284@qq.com>
2019-11-27 19:26:08 +0800

    work13

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ const openDefaultBrowser=function(url){
     var exec=require('child_process').exec;
     switch (process.platform){
         case"darwin":
-        exec('open'+url);
-        break;
+        exec('open '+url);
+        break; 
         case"win32":
-        exec('start'+url);
+        exec('start '+url);
         break;
         default:
             exec('xdg-open',[url]);
