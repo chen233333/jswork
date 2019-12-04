1914693f63c13ce986e920ee07d22bebdee15c4a
chen233333 <858788284@qq.com>
2019-12-04 13:15:51 +0800

    d

--- work14/index2.js
+++ work14/index2.js
@@ -1,26 +1,18 @@
-let endseconds = new Date().getTime() ;
-let d = h = m = s = 0;
-let id = setInterval(seckill, 1000);
+let time = new Date()
+let minutes = seconds = 0;
+minutes = 59 - time.getMinutes()
+seconds = 59 - time.getSeconds()
+let id0 =setInterval(seckill,1000)
 
 function seckill() {
-    let nowtime = new Date();
-    let remaining = parseInt((endseconds - nowtime.getTime()) / 1000);
-    if(remaining > 0) {
-        d = parseInt(remaining / 86400);
-        h = parseInt((remaining / 3600) % 24);
-        m = parseInt((remaining / 60) % 60);
-        s = parseInt(remaining % 60);
-
-        d = d < 10 ? '0' + d : d;
-        h = h < 10 ? '0' + h : h;
-        m = m < 10 ? '0' + m : m;
-        s = s < 10 ? '0' + s : s;
-    }else {
-        clearInterval(id);
-        d = h = m = s = '00';
-    }
-    document.getElementById('d').innerHTML = d + '天';
-    document.getElementById('h').innerHTML = h + '时';
-    document.getElementById('m').innerHTML = m + '分';
-    document.getElementById('s').innerHTML = s + '秒';
+    seconds--
+    if(seconds == -1){
+        seconds = 59
+        minutes--
+    } 
+    if(minutes == -1){
+        minutes = 59
+    } 
+    document.getElementById('minutes').innerHTML = minutes +'分'
+    document.getElementById('seconds').innerHTML = seconds +'秒'
 }
